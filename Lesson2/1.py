40a48703ac2497e65d794aa75d725084e8f90990
sulaimansyed811-alt <sulaimansyed@gmail.com>
2026-07-23 14:14:12 +0300

    Lesson2 completed

---
+++ Lesson2/1.py
@@ -0,0 +1,7 @@
+print("this is my second Codingal class")
+name="Sulaiman"
+print(name)
+print("name")
+print("My name is",name)
+print(f"My name is {name}")
+print(4)
---
+++ Lesson2/2.py
@@ -0,0 +1,7 @@
+x=4
+y=5
+print("x")
+print("y")
+print("The value of x is",x)
+print("The value of y is",y)
+print("The value of x+y",x+y)
